8a57b772013bc0c1d00e541ed54b6fe445449e6e
fist_it <fist_it@protonmail.com>
2025-11-28 12:25:04 +0100

    feat(map): Basic Leaflet map

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
 def main():
-    print("Hello from all-in-best2025!")
+    # Uruchomienie serwera w trybie deweloperskim
+    app.run(debug=True)
 
 
-if __name__ == "__main__":
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
     main()
